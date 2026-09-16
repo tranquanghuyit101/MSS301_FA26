90f48a18b65068221ce96338842146782683b5ce
tranquanghuyit101 <tranquanghuyit101@gmail.com>
2026-09-16 14:19:39 +0700

    feat(service): implement ProductService logic

---
+++ Slot3/product-service/src/main/java/com/fudn/product_service/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.fudn.product_service.service;
+
+import com.fudn.product_service.dto.ProductRequest;
+import com.fudn.product_service.dto.ProductResponse;
+import com.fudn.product_service.model.Product;
+import com.fudn.product_service.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+
+        Product savedProduct = productRepository.save(product);
+        log.info("Product {} is saved", savedProduct.getId());
+
+        return mapToProductResponse(savedProduct);
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream()
+                .map(this::mapToProductResponse)
+                .toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
