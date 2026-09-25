aacf5efab50075bb9d032582788d2e056ab3cfa0
tranquanghuyit101 <tranquanghuyit101@gmail.com>
2026-09-25 16:06:54 +0700

    feat(inventory-service): add inventory-service module in ShoppingServices

---
+++ Slot6/ShoppingServices/inventory-service/src/main/java/com/fudn/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.fudn.inventoryservice.repository;
+
+import com.fudn.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
