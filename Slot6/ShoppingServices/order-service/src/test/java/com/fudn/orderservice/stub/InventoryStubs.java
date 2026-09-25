d13b826015ce292f07d0ca17eeba8d145a760a91
tranquanghuyit101 <tranquanghuyit101@gmail.com>
2026-09-25 16:08:09 +0700

    test(order-service): add WireMock stubs and integration test for OpenFeign

---
+++ Slot6/ShoppingServices/order-service/src/test/java/com/fudn/orderservice/stub/InventoryStubs.java
@@ -0,0 +1,18 @@
+package com.fudn.orderservice.stub;
+
+import lombok.experimental.UtilityClass;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+@UtilityClass
+public class InventoryStubs {
+
+    public void stubInventoryCall(String skuCode, Integer quantity) {
+        stubFor(get(urlPathEqualTo("/api/inventory"))
+                .withQueryParam("skuCode", equalTo(skuCode))
+                .withQueryParam("quantity", equalTo(String.valueOf(quantity)))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("true")));
+    }
+}
